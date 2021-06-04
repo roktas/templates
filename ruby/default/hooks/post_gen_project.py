87c1f87660e95b6b7413b5aa680ae7893e692bbb
Recai Oktaş <roktas@gmail.com>
2021-06-05 01:59:40 +0300

    .

--- ruby/default/hooks/post_gen_project.py
+++ ruby/default/hooks/post_gen_project.py
@@ -1,3 +1,18 @@
-license = '{{ cookiecutter.license }}'
+import json
+import urllib.request
+
+LICENSES = {
+    "CC0-1.0": "cc0-1.0",
+    "GPL-3.0": "gpl-3.0",
+    "MIT":     "mit",
+    "MPL-2.0": "mpl-2.0",
+}
+
+license  = '{{ cookiecutter.license }}'
+endpoint = LICENSES[license]
+
+json = urllib.request.urlopen('http://api.github.com/licenses/{}'.format(endpoint)).read()
+
+# TODO
 
 print(license)
